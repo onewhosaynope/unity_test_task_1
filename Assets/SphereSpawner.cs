1bea19e2b631cd8d1c7bb076b0eb4e93f54c5cda
Vsevolod <onewhosaynope@gmail.com>
2020-01-17 21:37:58 +0300

    initial commit

---
+++ Assets/SphereSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit hit;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            if(Input.GetKey(KeyCode.Mouse0)) {
+                GameObject obj = Instantiate(prefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+            }
+        }
+    }
+}
