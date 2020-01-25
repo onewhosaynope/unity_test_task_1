ac2d8ca85b978199d4b0412240acfb57defa4132
Vsevolod <onewhosaynope@gmail.com>
2020-01-25 14:34:54 +0300

    updated script

--- Assets/SphereSpawner.cs
+++ Assets/SphereSpawner.cs
@@ -17,32 +17,29 @@ public class SphereSpawner : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if(Input.GetButtonDown("Fire1")) {
-
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            
             if (Physics.Raycast(ray, out hit)) {
-            
                 Vector3 target = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-                
                 // Debug.DrawLine(Camera.main.transform.position, target, Color.blue, 100);
                 // Debug.DrawLine(Camera.main.transform.position, target * prefabSpeed, Color.red, 100);
-                
                 // Debug.DrawLine(Camera.main.transform.position, target.normalized, Color.green, 100);
                 // Debug.DrawLine(Camera.main.transform.position, target.normalized * prefabSpeed, Color.yellow, 100);
-
-                Vector3 test = new Vector3(target.x, target.y, target.z);
-                Vector3 testSpeed = new Vector3(target.x, target.y * prefabSpeed, target.z);
-                
+                // Vector3 test = new Vector3(target.x, target.y, target.z);
+                // Vector3 testSpeed = new Vector3(target.x, target.y * prefabSpeed, target.z);
                 GameObject projectile = Instantiate(prefab, Camera.main.transform.position, Camera.main.transform.rotation) as GameObject;
-                
-                projectile.GetComponent<Rigidbody>().AddForce(testSpeed, ForceMode.Impulse);
+                projectile.GetComponent<Rigidbody>().AddForce(target * prefabSpeed, ForceMode.Impulse);
+            }
+        }
+
+        if(Input.GetButtonDown("Fire2")) {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                Instantiate(prefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity)
             }
         }
 
         if (Input.GetButtonDown("Jump")) {
-        
             Instantiate(prefab, Camera.main.transform.position, Camera.main.transform.rotation);
-        
         }
     }
 }
